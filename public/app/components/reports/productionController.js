c0d461da7a48c57d037d4528495a7d6dad1446b9
isotopeee <johnmichael.doroy@gmail.com>
2017-06-28 17:09:02 +0800

    Code refactor and optimized functionality

--- public/app/components/reports/productionController.js
+++ public/app/components/reports/productionController.js
@@ -1,46 +1,47 @@
-angular
-    .module('altairApp')
+(function() {
+  'use strict';
+  angular
+    .module('dietviewApp')
     .controller('productionCtrl', [
-        '$scope',
-        'production_data',
-        function ($scope, production_data) {
+      '$scope',
+      'production_data',
+      function($scope, production_data) {
+        $scope.production_list = [];
+        var today = new Date();
+        var tod = today.getDate();
+        $scope.date = { today: today.toLocaleDateString() }
 
-          $scope.production_list = [];
+        $scope.exportToPDF = exportToPDF;
 
-          console.log(production_data);
+        ////////////////////////////////////////////////////////////////////////
 
-          var today = new Date();
-          var tod = today.getDate();
-          $scope.date = {
-            today: today
-          }
-
-          //filter production
-          for(var i = 0 ; i < production_data.length ; i++) {
-            if (production_data[i].status === 'active') {
-              var startDate = new Date(production_data[i].startDate);
-              var endDate = new Date(production_data[i].endDate);
-              var start = startDate.getDate();
-              var indexx = tod - start;
-              if(startDate < today && today < endDate)
-              {
-                var production = {
-                  breakfast : production_data[i].mealPlan.meals[indexx].breakfast,
-                  lunch : production_data[i].mealPlan.meals[indexx].lunch,
-                  dinner : production_data[i].mealPlan.meals[indexx].dinner,
-                  snack : production_data[i].mealPlan.meals[indexx].snack
-                }
-                $scope.production_list.push(production);
-              }
-              console.log($scope.production_list);
-            }
-
-          }
-
-            $scope.exportAction = function(){
-              $scope.$broadcast('export-pdf', {});
-
-            }
-
+        function activate() {
+          _filterProductionList();
         }
+
+        function exportToPDF() {
+          // TODO: Export production list to pdf
+          // NOTE: Refer to https://stackoverflow.com/questions/21628378/angularjs-display-blob-pdf-in-an-angular-app
+        }
+
+        function _filterProductionList() {
+          for (var i = 0; i < production_data.length; i++) {
+            var startDate = new Date(production_data[i].startDate);
+            var endDate = new Date(production_data[i].endDate);
+            var start = startDate.getDate();
+            var indexx = tod - start;
+            if (startDate < today && today < endDate) {
+              var production = {
+                breakfast: production_data[i].mealPlan.meals[index].breakfast,
+                lunch: production_data[i].mealPlan.meals[index].lunch,
+                dinner: production_data[i].mealPlan.meals[index].dinner,
+                snack: production_data[i].mealPlan.meals[index].snack
+              };
+              $scope.production_list.push(production);
+            }
+            console.log($scope.production_list);
+          }
+        }
+      }
     ]);
+}());
